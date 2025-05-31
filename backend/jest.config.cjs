46220d0f70bffcef8c3bb2585553ccf4b8daf4af
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2025-05-31 19:42:41 +0200

    CI adjustments

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src/tests'],
+  roots: ['<rootDir>/tests'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
